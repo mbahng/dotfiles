e06fd02deae1db0ea714430a62d8c6a08f8c7f24
Muchang Bahng <bahngmc@gmail.com>
2024-10-16 23:43:16 -0400

    fixed nvim-treesitter deprecation warning
    whoops need to add this

--- editor/nvim/lua/user/autopairs.lua
+++ editor/nvim/lua/user/autopairs.lua
@@ -1,4 +1,3 @@
--- Setup nvim-cmp.
 local status_ok, npairs = pcall(require, "nvim-autopairs")
 if not status_ok then
   return
@@ -13,19 +12,3 @@ npairs.setup {
   },
   disable_filetype = { "TelescopePrompt" },
 }
-
--- need this section to autocomplete <html> tags
-local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then
-  return
-end
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
-
-require'nvim-treesitter.configs'.setup {
-  autotag = {
-    enable = true,
-  }
-}
--- 
-
