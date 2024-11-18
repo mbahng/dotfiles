a8eaedbec2e386d25dc41bc2606d99143b4b6dde
Muchang Bahng <bahngmc@gmail.com>
2024-11-17 23:49:30 -0500

    updated treesitter from deprecated versions

--- editor/nvim/lua/user/treesitter.lua
+++ editor/nvim/lua/user/treesitter.lua
@@ -1,7 +1,9 @@
-local configs = require("nvim-treesitter.configs")
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
 configs.setup {
   ensure_installed = {
-    "python", 
     "c", 
     "cpp", 
     "lua", 
@@ -9,6 +11,7 @@ configs.setup {
     "html", 
     "css", 
     "javascript", 
+    "jsdoc", 
     "typescript", 
     "json", 
     "yaml"
