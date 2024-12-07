db77e6f06aad19eeeb10484de02bf813a0673074
Muchang Bahng <bahngmc@gmail.com>
2024-12-06 21:30:04 -0500

    better toggleterm stuff

--- editor/nvim/lua/user/toggleterm.lua
+++ editor/nvim/lua/user/toggleterm.lua
@@ -32,16 +32,65 @@ toggleterm.setup({
 	},
 })
 
-function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  -- vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+local Terminal = require('toggleterm.terminal').Terminal
+
+-- Create three fixed terminals
+local term1 = Terminal:new({
+  count = 1,
+  direction = 'float',
+  float_opts = {
+    border = 'curved'
+  }
+})
+
+local term2 = Terminal:new({
+  count = 2,
+  direction = 'float',
+  float_opts = {
+    border = 'curved'
+  }
+})
+
+local term3 = Terminal:new({
+  count = 3,
+  direction = 'float',
+  float_opts = {
+    border = 'curved'
+  }
+})
+
+-- Function to cycle right (next terminal)
+function _G.cycle_terminal_right()
+  local current_term = vim.b.toggle_number
+  local next_term = current_term + 1
+  if next_term > 3 then
+    next_term = 1
+  end
+  vim.cmd('close')
+  vim.cmd(next_term .. 'ToggleTerm')
 end
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+-- Function to cycle left (previous terminal)
+function _G.cycle_terminal_left()
+  local current_term = vim.b.toggle_number
+  local prev_term = current_term - 1
+  if prev_term < 1 then
+    prev_term = 3
+  end
+  vim.cmd('close')
+  vim.cmd(prev_term .. 'ToggleTerm')
+end
 
-local Terminal = require("toggleterm.terminal").Terminal
+-- Set up terminal keymaps
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  -- vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-`>', [[<C-\><C-n>]], opts)
+  -- Navigate between terminals
+  vim.keymap.set('t', '<C-l>', '<cmd>lua cycle_terminal_right()<CR>', opts)
+  vim.keymap.set('t', '<C-h>', '<cmd>lua cycle_terminal_left()<CR>', opts)
+end
+
+-- Auto-set keymaps when terminal opens
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+
