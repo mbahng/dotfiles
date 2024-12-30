cf330f600a3e3c229340cc3c83feb6ac6d47ea04
Muchang Bahng <bahngmc@gmail.com>
2024-12-30 16:24:25 -0500

    updated fold methods and shiftwidth in markdown files

--- editor/nvim/ftplugin/markdown.lua
+++ editor/nvim/ftplugin/markdown.lua
@@ -1,7 +1,18 @@
 vim.cmd "set wrap"
 vim.cmd "setlocal spell spelllang=en_us"
 vim.cmd "set breakindent"
-
 vim.keymap.set("n", "<leader>s", ":MarkdownPreviewToggle<cr>")
+vim.cmd "set foldmethod=expr"
+vim.cmd "set foldexpr=v:lua.markdown_level()"
+vim.cmd "set shiftwidth=3"
 
-
+-- Define the Lua function
+function _G.markdown_level()
+    local line = vim.fn.getline(vim.v.lnum)
+    local header_level = line:match("^(##+)")
+    if header_level then
+        return ">" .. #header_level
+    else
+        return "="
+    end
+end
