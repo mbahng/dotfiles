13bf950f4dbbb55503f248064939e1696f54c9e7
Muchang Bahng <bahngmc@gmail.com>
2025-01-09 12:49:26 -0500

    removed comments from neotree config

--- editor/nvim/lua/user/plugins.lua
+++ editor/nvim/lua/user/plugins.lua
@@ -75,9 +75,8 @@ return packer.startup(function(use)
       branch = "v3.x",
       requires = { 
         "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "nvim-tree/nvim-web-devicons", 
         "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
       }
   }
 
