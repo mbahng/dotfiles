6429208dbe53b023f1ffc399152bc0c3e135c358
Muchang Bahng <bahngmc@gmail.com>
2025-05-28 22:59:33 -0400

    updates to markdown

--- editor/nvim/ftplugin/markdown.lua
+++ editor/nvim/ftplugin/markdown.lua
@@ -1,18 +1,8 @@
+vim.g.markdown_recommended_style = 0
 vim.cmd "set wrap"
+vim.cmd "set linebreak"
 vim.cmd "setlocal spell spelllang=en_us"
-vim.cmd "set breakindent"
 vim.keymap.set("n", "<leader>s", ":MarkdownPreviewToggle<cr>")
-vim.cmd "set foldmethod=expr"
-vim.cmd "set foldexpr=v:lua.markdown_level()"
 vim.cmd "set shiftwidth=3"
-
--- Define the Lua function
-function _G.markdown_level()
-    local line = vim.fn.getline(vim.v.lnum)
-    local header_level = line:match("^(##+)")
-    if header_level then
-        return ">" .. #header_level
-    else
-        return "="
-    end
-end
+vim.cmd "set foldmethod=indent"
+vim.cmd "TSEnable highlight"
