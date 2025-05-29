47f4dad6758e76cd594592f78fa81bfcdb2c9b97
Muchang Bahng <bahngmc@gmail.com>
2025-05-28 22:59:52 -0400

    updates to tex, fixed comment bug where % appears as first char

--- editor/nvim/ftplugin/tex.lua
+++ editor/nvim/ftplugin/tex.lua
@@ -12,11 +12,10 @@ local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 
-keymap("v", "<leader>c", ":s/^/%/<cr>:noh<cr>", opts)
-keymap("n", "<leader>c", "v:s/^/%/<cr>:noh<cr>", opts)
+-- keymap("v", "<leader>c", ":s/^/%/<cr>:noh<cr>", opts)
 keymap("n", "<leader>s", ":VimtexCompile<cr>", opts)
-keymap("n", "<leader>r", ":VimtexTocToggle<cr>", opts)
-keymap("n", "<C-f>", ":.!inkscape-figures create fig/", opts)
+-- keymap("n", "<leader>r", ":VimtexTocToggle<cr>", opts)
+-- keymap("n", "<C-f>", ":.!inkscape-figures create fig/", opts)
 -- keymap("n", "<C-e>", ":.!inkscape-figures edit figures/", opts)
 -- inoremap <C-f> <Esc>: silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>
 -- nnoremap <C-f> : silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>
