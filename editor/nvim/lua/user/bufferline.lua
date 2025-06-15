9652c34098d33383b589ede58bfa36c680677cef
mbahng <bahngmc@gmail.com>
2025-06-15 11:49:20 -0400

    iron keymaps and iron repls are now buffers
    iron repls are now buffers
    
    iron keymaps

--- editor/nvim/lua/user/bufferline.lua
+++ editor/nvim/lua/user/bufferline.lua
@@ -25,21 +25,27 @@ bufferline.setup {
       return "("..count..")"
     end,
     -- NOTE: this will be called a lot so don't do any heavy processing here
-    -- custom_filter = function(buf_number)
-    --   -- filter out filetypes you don't want to see
-    --   if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-    --     return true
-    --   end
-    --   -- filter out by buffer name
-    --   if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-    --     return true
-    --   end
-    --   -- filter out based on arbitrary rules
-    --   -- e.g. filter out vim wiki buffer from tabline in your work repo
-    --   if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-    --     return true
-    --   end
-    -- end,
+    custom_filter = function(buf_number)
+      local buftype = vim.bo[buf_number].buftype
+      local bufname = vim.fn.bufname(buf_number)
+      
+      -- Include Iron REPL terminal buffers
+      if buftype == "terminal" and (string.match(bufname, "iron") or vim.bo[buf_number].filetype == "iron") then
+        return true
+      end
+      
+      -- Exclude other terminal buffers (optional)
+      if buftype == "terminal" then
+        return false
+      end
+      
+      -- Exclude other unwanted buffer types
+      if buftype == "quickfix" or buftype == "help" or buftype == "nofile" then
+        return false
+      end
+      
+      return true
+    end,
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
     show_buffer_icons = true,
     show_buffer_close_icons = true,
@@ -148,3 +154,23 @@ bufferline.setup {
     },
   },
 }
+
+-- Make Iron REPL buffers listed
+vim.api.nvim_create_autocmd({"TermOpen", "BufEnter"}, {
+  pattern = "*",
+  callback = function()
+    local bufname = vim.fn.bufname()
+    local buftype = vim.bo.buftype
+    
+    -- Check if this is an Iron REPL buffer
+    if buftype == "terminal" and (
+      string.match(bufname, "iron") or 
+      string.match(bufname, "python") or 
+      string.match(bufname, "ipython") or
+      string.match(bufname, "julia") or
+      string.match(bufname, "R")  -- adjust patterns as needed
+    ) then
+      vim.bo.buflisted = true
+    end
+  end,
+})
