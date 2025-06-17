a58ffc4021f72d37040c1e2102fb079cd056c510
Muchang Bahng <bahngmc@gmail.com>
2025-06-17 00:42:21 -0400

    language support reduced to save computation and latency

--- editor/nvim/lua/user/bufferline.lua
+++ editor/nvim/lua/user/bufferline.lua
@@ -20,7 +20,7 @@ bufferline.setup {
     max_name_length = 15,
     max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
     tab_size = 15,
-    diagnostics = "nvim_lsp", 
+    diagnostics = "none",
     diagnostics_update_in_insert = false,
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
       return "("..count..")"
