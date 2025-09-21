d98479dcaaf901b6a31341d3b20afbe63cab93f2
Muchang Bahng <bahngmc@gmail.com>
2025-09-21 16:34:56 -0400

    background of kitty should be the same as colorscheme

--- editor/nvim/init.lua
+++ editor/nvim/init.lua
@@ -26,6 +26,16 @@ require "user.diffview"
 require "user.dadbod"
 require "user.dap"
 
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then return end
+    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+  end,
+})
 
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function() io.write("\027]111\027\\") end,
+})
 vim.g.loaded_perl_provider = 0
 vim.cmd "hi WinSeparator guifg=#ffffff"
