405ed4b7273fc8e8d037ff242b34db124e84c0bd
Muchang Bahng <bahngmc@gmail.com>
2025-09-21 17:52:23 -0400

    nvim dap testing

--- editor/nvim/lua/user/dap.lua
+++ editor/nvim/lua/user/dap.lua
@@ -2,9 +2,41 @@ local dap = require("dap")
 local dapui = require("dapui")
 local dap_python = require("dap-python")
 
-require("dapui").setup({})
+require("dapui").setup({
+  expand_lines = false, 
+  layouts = {
+    {
+      elements = {
+        -- You can remove any of these you don't want
+        { id = "scopes", size = 0.75 },      -- Variables in current scope
+        -- { id = "breakpoints", size = 0.25 }, -- List of breakpoints
+        { id = "stacks", size = 0.25 },      -- Call stack
+        -- { id = "watches", size = 0.25 },     -- Watch expressions
+      },
+      size = 80,  -- Width of the sidebar
+      position = "left",
+    },
+    {
+      elements = {
+        { id = "console", size = 1.0 },      -- Program output
+      },
+      size = 10,  -- Height of bottom panel
+      position = "bottom",
+    },
+  },
+})
 require("nvim-dap-virtual-text").setup({
-  commented = true, -- Show virtual text alongside comment
+  commented = false,
+  -- virt_text_pos = 'eol',  -- Position: 'eol' | 'overlay' | 'right_align'
+  virt_lines = true,    
+  display_callback = function(variable, buf, stackframe, node, options)
+    if #variable.value > 50 then
+      local first_part = string.sub(variable.value, 1, 30)
+      local last_part = string.sub(variable.value, -20)
+      return ' ' .. first_part .. '...' .. last_part
+    end
+    return ' ' .. variable.value
+  end,
 })
 
 dap_python.setup("python3")
@@ -50,6 +82,14 @@ end, opts)
 -- Step Over
 vim.keymap.set("n", "<leader>do", function()
   dap.step_over()
+end, opts) 
+vim.keymap.set("n", "<S-o>", function()
+  dap.step_over()
+end, opts) 
+
+-- expand variable
+vim.keymap.set("n", "<leader>dk", function()
+  dapui.eval(nil, { enter = true })
 end, opts)
 
 -- Step Into
@@ -70,4 +110,17 @@ end, opts)
 -- Toggle DAP UI
 vim.keymap.set("n", "<leader>du", function()
   dapui.toggle()
+end, opts)
+
+-- Toggle specific element
+vim.keymap.set("n", "<leader>dS", function()
+  require("dapui").toggle({ element = "scopes" })
+end, opts)
+
+vim.keymap.set("n", "<leader>dR", function()
+  require("dapui").toggle({ element = "repl" })
+end, opts)
+
+vim.keymap.set("n", "<leader>dW", function()
+  require("dapui").toggle({ element = "watches" })
 end, opts)
